311679c8a48c37abb8f5830f2c2d1382e7bdf9d3
cecilelecerf <c.lecerf.cecile@gmail.com>
2025-07-18 00:55:48 +0200

    feat: es6 and prettier for middlwares

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,2 +1,2 @@
-export * from './session.middleware';
-export * from './role.middleware';
+export * from "./session.middleware";
+export * from "./role.middleware";
--- middlewares/roles.middleware.ts
+++ middlewares/roles.middleware.ts
@@ -1,16 +1,15 @@
 import { Request, RequestHandler } from "express";
 import { UserRole } from "../models/user.interface";
 
-export function rolesMiddleware(allowedRoles: UserRole[]): RequestHandler {
-    return (req: Request, res, next) => {
-        if (!req.user) {
-            return res.status(401).json({ error: "Utilisateur non authentifié" });
-        }
+export const rolesMiddleware = (allowedRoles: UserRole[]): RequestHandler => {
+  return (req: Request, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ error: "Utilisateur non authentifié" });
+    }
 
-        if (!allowedRoles.includes(req.user.role)) {
-            return res.status(403).json({ error: "Accès refusé" });
-        }
-
-        next();
-    };
-}
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ error: "Accès refusé" });
+    }
+    next();
+  };
+};
